86f2531dc08cbc2b60c9c106faf8a44ae942186e
NebulaEclipse <chrisdouglas200516@gmail.com>
2024-03-27 21:33:49 -0400

    Added code to database and install in js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,7 +1,7 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB('jateDB', 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
@@ -13,9 +13,23 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const jateDB = await openDB('jateDB', 1);
+  const tx = jateDB.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.add({ value: content });
+  const result = await request;
+  return result;
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const jateDB = await openDB('jateDB', 1)
+  const tx = jateDB.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  return result.value;
+};
 
 initdb();
